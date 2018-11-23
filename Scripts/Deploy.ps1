f16d54c0fd1141ed703324d341c8082b6771f9a7
Vajira Kulatunga <vajira.kulatunga@pearson.com>
2018-11-23 23:24:13 +0530

    Add Powershell script

---
+++ Scripts/Deploy.ps1
@@ -0,0 +1,6 @@
+﻿
+Write-Host "Removing old package"
+Remove-Item "C:\Vajira\Navy\IIS" -Force -Recurse
+Write-Host "Deploy new package"
+Copy-Item "C:\Vajira\Navy\NavyRepo\NavySampleSite\NavySampleSite" -Recurse -Destination "C:\Vajira\Navy\IIS\NavySampleSite"
+Write-Host "Deployment completed"
