10a0e55125a41ac89f28f2d820ad3b6d8fead05c
Vajira Kulatunga <vajira.kulatunga@pearson.com>
2018-11-23 23:42:13 +0530

    Corrected source path

--- Scripts/Deploy.ps1
+++ Scripts/Deploy.ps1
@@ -3,6 +3,6 @@ Write-Host "********************************"
 Write-Host "Removing old package"
 Remove-Item "C:\Vajira\Navy\IIS" -Force -Recurse
 Write-Host "Deploy new package"
-Copy-Item "C:\Vajira\Navy\NavyRepo\NavySampleSite\NavySampleSite" -Recurse -Destination "C:\Vajira\Navy\IIS\NavySampleSite"
+Copy-Item "C:\Vajira\Navy\Jenkins\workspace\One\NavySampleSite\NavySampleSite" -Recurse -Destination "C:\Vajira\Navy\IIS\NavySampleSite"
 Write-Host "Deployment completed"
 Write-Host "********************************"
